ead6ddbcf57f029c7b111bf7b9e0a3bbbc2f3bc8
Jan Fojtík <95308667+H0lyP0tat0@users.noreply.github.com>
2025-02-08 14:30:27 +0100

    updated request handling
    Took 23 minutes

--- prolog/rules.pl
+++ prolog/rules.pl
@@ -32,28 +32,6 @@ eq_elim([eq(A, B)], imp(B, A)).
 
 % Generic rule checker
 prove_handler(Premises, Conclusion, Rule) :-
-    atom_string(RuleAtom, Rule),
-    term_string(ConcTerm, Conclusion),
-    maplist(atom_string, PremAtoms, Premises),
-
-    % Check rule and apply it
-    rule(RuleAtom, PremiseCount, Predicate),
-    length(PremAtoms, PremiseCount),
-
-    % Unify the conclusion term with the premises
-    check_instantiated(PremAtoms, ConcTerm),
-
-    % Apply the rule, unify the terms correctly
-    call(Predicate, PremAtoms, ConcTerm).
-
-% Check that all variables in the conclusion are instantiated by the premises
-check_instantiated(ConcTerm, Premises) :-
-    term_variables(ConcTerm, ConclusionVars),
-    % Ensure that each variable in the conclusion is instantiated in the premises
-    forall(member(Var, ConclusionVars), check_var_in_premises(Var, Premises)).
-
-% Check that a variable is present in the premises (either as a term or a variable)
-check_var_in_premises(Var, Premises) :-
-    (   var(Var) -> true  % If it's a variable, we don't need to check it explicitly
-    ;   member(Var, Premises)  % Otherwise, check that the term matches an atom in the premises
-    ).
+    rule(Rule, PremiseCount, Predicate),
+    length(Premises, PremiseCount),
+    call(Predicate, Premises, Conclusion).
--- prolog/server.pl
+++ prolog/server.pl
@@ -17,13 +17,20 @@ prove_request(Request) :-
     % Read JSON data
     http_read_json_dict(Request, Data),
 
+    maplist(string_to_term, Data.premises, Premises),
+    string_to_term(Data.conclusion, Conclusion),
+    atom_string(Rule, Data.rule),
+
     % Apply the rule
-    (  prove_handler(Data.premises, Data.conclusion, Data.rule)
+    (  prove_handler(Premises, Conclusion, Rule)
     -> Reply = json([success =  true, premises = Data.premises, conclusion = Data.conclusion, rule = Data.rule])
     ;  Reply = json([success = false, premises = Data.premises, conclusion = Data.conclusion, rule = Data.rule])
     ),
 
     reply_json(Reply).
+
+string_to_term(String, Term) :-
+    read_term_from_atom(String, Term, []).
 
 start_server(Port) :-
     http_server(http_dispatch, [port(Port)]).
